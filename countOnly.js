cc3cd6c41b180419860ae8d675b62f0fd9ced79e
Russell Cape <russellcape98@gmail.com>
2019-10-16 19:52:43 +0000

    arrays and objects passed

--- countOnly.js
+++ countOnly.js
@@ -8,21 +8,25 @@ const assertEqual = function(actual, expected) {
 
 // allItems: an array of strings that we need to look through
 // itemsToCount: an object specifying what to count
+
 const countOnly = function(allItems, itemsToCount) {
   const results = {};
-
   for (const item of allItems) {
-    if (itemsToCount[item] === true) {
-    console.log(item + ":", )
-    if (results[item] !== undefined) {
-      results[item] = results[item] + 1// add 1
-    } else {
-      results[item] = 1 // add item to results
+
+    if (itemsToCount[item]) {
+      
+      if (results[item]) {
+        results[item] += 1; // add 1
+      } else {
+        results[item] = 1; // add item to results
       }
+      console.log(item);
     }
+
   }
-  return results
-}
+  return results;
+};
+
 const firstNames = [
   "Karl",
   "Salima",
