92412f8cf19c49990e2e1e5d39085a4f2592ec5d
Russell Cape <russellcape98@gmail.com>
2019-10-17 19:07:34 +0000

    assert arrays (fixed)

--- assertArraysEqual.js
+++ assertArraysEqual.js
@@ -10,7 +10,7 @@ const eqArrays = function(array1, array2) {
 };
 
 const assertArraysEqual = function(actual, expected) {
-  if (eqArrays(actual, expected)) {
+  if (actual === expected) {
     console.log(`✅✅✅ Assertion Passed: ${actual} === ${expected}`);
   } else {
     console.log(`🛑🛑🛑 Assertion Failed: ${actual} !==  ${expected}`);
