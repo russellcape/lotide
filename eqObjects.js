ec6cadb6805f39a97e545d81af165eeb6fefaf22
Russell Cape <russellcape98@gmail.com>
2019-10-17 00:28:36 +0000

    object => array => assertion

--- eqObjects.js
+++ eqObjects.js
@@ -18,8 +18,6 @@ const eqArrays = function(array1, array2) {
 };
 
 
-// Returns true if both objects have identical keys with identical values.
-// Otherwise you get back a big fat false!
 const eqObjects = function(object1, object2) {
   if (Object.keys(object1).length !== Object.keys(object2).length) {
     return false;
