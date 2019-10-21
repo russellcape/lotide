b6ad2a9a2848e075b8b66a756dac3c148c47eb35
Russell Cape <russellcape98@gmail.com>
2019-10-21 19:33:34 +0000

    Export tail

--- tail.js
+++ tail.js
@@ -1,17 +1,5 @@
-const assertEqual = function(actual, expected) {
-  if (actual === expected) {
-    console.log(`✅✅✅ Assertion Passed: ${actual} === ${expected}`);
-  } else {
-    console.log(`🛑🛑🛑 Assertion Failed: ${actual} !==  ${expected}`);
-  }
-};
-
 const tail = function(array) {
-  console.log(array.slice());
+  return array.slice();
 };
 
-const array = ["Yo Yo", "Lighthouse", "labs"];
-
-
-tail(array);
-assertEqual(array.length, 3);
+module.exports = tail;
