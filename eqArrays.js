de04cc31fa33ba7049b61862244085228436ec22
Russell Cape <russellcape98@gmail.com>
2019-10-21 20:49:48 +0000

    edited to read array2 aswell

--- eqArrays.js
+++ eqArrays.js
@@ -2,12 +2,12 @@ const eqArrays = function(array1, array2) {
   if (array1.length !== array2.length) {
     return false
   }
-  for (let i = 0; i < array1.length; i++) {
-    if (array1[i] !== array2[i])
+  for (let i = 0; i < array1.length && i < array2.length; i++) {
+    if (array1[i] !== array2[i]) {
     return false
-  }
-    return true
+    }
+  } 
+  return true
 };
-
 
 module.exports = eqArrays;
