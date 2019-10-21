e86f9dff9390514361f22b65e968c9f31d0b6f80
Russell Cape <russellcape98@gmail.com>
2019-10-21 21:02:01 +0000

    All assertions pass

--- test/assertEqualTest.js
+++ test/assertEqualTest.js
@@ -1,8 +1,6 @@
 const assertEqual = require('../assertEqual');
 
 
-assertEqual("Lighthouse Labs", "Bootcamp");
+assertEqual("Lighthouse Labs", "Lighthouse Labs");
 assertEqual(1, 1);
-assertEqual("Russ", "Russ");
-assertEqual(1, 8);
 
