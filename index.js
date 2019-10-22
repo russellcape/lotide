11f60ad8546db3f9b7aed9ae57b440bd14d82fa8
Russell Cape <russellcape98@gmail.com>
2019-10-22 23:41:03 +0000

    Index of exports that ar being used... the rest is commented out

--- index.js
+++ index.js
@@ -1,15 +1,44 @@
 const head   = require('./head');
 const tail   = require('./tail');
 const middle = require('./middle');
-const eqArrays = require('./eqArrays');
-const assertEqual = require('./assertEqual');
-const assertArraysEqual = require('./assertArraysEqual');
+const index = require('./index');
 
 module.exports = {
   head:   head,
   tail:   tail,
   middle: middle,
-  eqArrays: eqArrays,
-  assertEqual: assertEqual,
-  assertArraysEqual: assertArraysEqual
+  index: index
 };
+
+
+// const countLetters = require('./countLetters');
+// const countOnly = require('./countOnly');
+// const eqArrays = require('./eqArrays');
+// const eqObjects = require('./eqObjects');
+// const findKey = require('./findKey');
+// const findKeyByValue = require('./findKeyByValue');
+// const flatten = require('./flatten');
+// const head = require('./head');
+// const letterPositions = require('./letterPositions');
+// const map = require('./map');
+// const middle = require('./middle');
+// const tail = require('./tail');
+// const takeUntil = require('./takeUntil');
+// const without = require('./without');
+
+// module.exports = {
+//   countLetters,
+//   countOnly,
+//   eqArrays,
+//   eqObjects,
+//   findKey,
+//   findKeyByValue,
+//   flatten,
+//   head,
+//   letterPositions,
+//   map,
+//   middle,
+//   tail,
+//   takeUntil,
+//   without,
+// };
